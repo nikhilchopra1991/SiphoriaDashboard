5b4b7953542322f4ec15df0db739be434c12bf06
vivekk251 <vivek.mahalingam32@gmail.com>
2017-07-14 12:50:41 +0530

    Create fetch_timeline_description.php

---
+++ fetch_timeline_description.php
@@ -0,0 +1,41 @@
+<?php
+
+
+require 'connect_to_db.php';
+$sql = "SELECT description  as P
+FROM news_feed
+ORDER BY timestamp_news LIMIT 12";
+
+
+$result = mysqli_query($conn, $sql);
+
+
+if (mysqli_num_rows($result) > 0) {
+
+while($row = mysqli_fetch_assoc($result)) {
+
+    echo $row['P']."<";
+
+
+
+    }
+
+
+}
+
+else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+
+
+
+
+
+
+
+
+
+?>
